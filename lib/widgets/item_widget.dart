2dc5e2c8db810a973d3ac13a04ffe63d0883e933
ahmedazim7804 <ahmedazim7804@gmail.com>
2023-11-02 19:07:34 +0530

    clickable url

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 import 'dart:math';
 
+import 'package:url_launcher/url_launcher.dart';
+
 class ProjectItem extends StatelessWidget {
   const ProjectItem({super.key, required this.name, required this.description, required this.techStacks, required this.gitRepoUrls, required this.imageUrl});
   final String? name;
@@ -9,6 +11,13 @@ class ProjectItem extends StatelessWidget {
   final List<dynamic> techStacks;
   final List<dynamic>? gitRepoUrls;
   final String? imageUrl;
+
+  Future<void> openLink(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri)) {
+      throw Exception('Could not launch $uri');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +30,7 @@ class ProjectItem extends StatelessWidget {
             Container(margin: const EdgeInsets.all(8),child: Text(name!, style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontSize: 28, fontWeight: FontWeight.bold))),
             Padding(padding: const EdgeInsets.symmetric(horizontal: 16),child: GridView.count(crossAxisSpacing: 10,childAspectRatio: 3/1,shrinkWrap: true,crossAxisCount: 3, children: techStacks.map((e) => ElevatedButton(onPressed: () {}, style: ElevatedButton.styleFrom(backgroundColor: Colors.primaries[Random().nextInt(Colors.primaries.length)].withAlpha(180)),child: Text(e, style: const TextStyle(color: Colors.black),),)).toList())),
             ListTile(leading: const Icon(Icons.description),title: Text(description!)),
-            ListTile(leading: const Icon(Icons.web_rounded),title: Text("Github Links", style: Theme.of(context).textTheme.titleMedium),subtitle: ListView.builder(shrinkWrap: true,itemBuilder: (ctx, index) => Text('${index+1}. ${gitRepoUrls![index]}', style: Theme.of(context).textTheme.titleSmall,),itemCount: gitRepoUrls!.length,))
+            ListTile(leading: const Icon(Icons.web_rounded),title: Text("Github Links", style: Theme.of(context).textTheme.titleMedium),subtitle: ListView.builder(shrinkWrap: true,itemBuilder: (ctx, index) => InkWell(onTap: (){openLink(gitRepoUrls![index]);},child: Text('${index+1}. ${gitRepoUrls![index]}', style: Theme.of(context).textTheme.titleSmall!.copyWith(color: Colors.blueGrey, decoration: TextDecoration.underline),)),itemCount: gitRepoUrls!.length,))
             ],
           ),
         ),
